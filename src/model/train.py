3d60bf5e18bed10108bbba535516f70c3f4f2fb7
MilesMayhew <bua9wn@virginia.edu>
2025-12-12 21:39:52 -0500

    Added script to train model on data

---
+++ src/model/train.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 32
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "hagrid/train",
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode="categorical"
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "hagrid/val",
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode="categorical"
+)
+
+data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip("horizontal"),
+    tf.keras.layers.RandomRotation(0.1),
+    tf.keras.layers.RandomZoom(0.1),
+])
+
+normalization = tf.keras.layers.Rescaling(1./255)
+
+base_model = tf.keras.applications.MobileNetV2(
+    input_shape=(224, 224, 3),
+    include_top=False,
+    weights="imagenet"
+)
+
+base_model.trainable = False  # transfer learning
+num_classes = len(train_ds.class_names)
+
+model = tf.keras.Sequential([
+    data_augmentation,
+    normalization,
+    base_model,
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dense(num_classes, activation="softmax") # 5 gestures
+])
+
+model.compile(
+    optimizer="adam",
+    loss="categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+history = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=10
+)
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    "hagrid/test",
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode="categorical"
+)
+
+test_loss, test_acc = model.evaluate(test_ds)
+print("Test accuracy:", test_acc)
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tf.float16]
+
+tflite_model = converter.convert()
+
+with open("hand_gesture.tflite", "wb") as f:
+    f.write(tflite_model)
